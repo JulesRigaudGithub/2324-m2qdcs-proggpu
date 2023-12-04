7673a46b3133692d5f6222b05fb5ab6305ef65ae
Jules Rigaud <Vjules.rigaud@student-cs.fr>
2023-12-04 01:39:30 +0100

    TP 2 Terminé

--- lab/lab2/matmul.cu
+++ lab/lab2/matmul.cu
@@ -2,7 +2,9 @@
 #include <iostream>
 #include "cuda.h"  
 
-#define N 512
+#include <chrono>
+
+#define N 1139
 
 // A and C are stored by rows, i.e., A(i, j) = A[i * N + j], C(i, j) = C[i * N + j]
 // B is stored by columns, i.e., B(i, j) = B[i + j * N]
@@ -23,7 +25,7 @@ __global__ void multiplyMatrixGPUByBlocks(float *dA, float *dB, float *dC, int n
   int row = blockIdx.y;
   float res = 0;
   for (int k = 0; k < n; k++) {
-    res += dA[row*N + k] * dB[k + col*N]
+    res += dA[row*N + k] * dB[k + col*N];
   }
   dC[row*n + col] = res;
 }
@@ -39,7 +41,7 @@ __global__ void multiplyMatrixGPUByBlocksThreads1D(float *dA, float *dB, float *
   int row = blockIdx.y;
   float res = 0;
   for (int k = 0; k < n; k++) {
-    res += dA[row*N + k] * dB[k + col*N]
+    res += dA[row*N + k] * dB[k + col*N];
   }
   dC[row*n + col] = res;
 }
@@ -58,7 +60,7 @@ __global__ void multiplyMatrixGPUByBlocksThreads1DNonMultiple(float *dA, float *
   int row = blockIdx.y;
   float res = 0;
     for (int k = 0; k < n; k++) {
-      res += dA[row*N + k] * dB[k + col*N]
+      res += dA[row*N + k] * dB[k + col*N];
     }
     dC[row*n + col] = res;
   }
@@ -77,7 +79,7 @@ __global__ void multiplyMatrixGPUByBlocksThreads2D(float *dA, float *dB, float *
   int row = (blockDim.y * blockIdx.y) + threadIdx.y;
   float res = 0;
   for (int k = 0; k < n; k++) {
-    res += dA[row*N + k] * dB[k + col*N]
+    res += dA[row*N + k] * dB[k + col*N];
   }
   dC[row*n + col] = res;
 }
@@ -95,7 +97,7 @@ __global__ void multiplyMatrixGPUByBlocksThreads2DNonMultiple(float *dA, float *
   if ((col < n) && (row < n)) {
     float res = 0;
     for (int k = 0; k < n; k++) {
-      res += dA[row*N + k] * dB[k + col*N]
+      res += dA[row*N + k] * dB[k + col*N];
     }
     dC[row*n + col] = res;
   }
@@ -149,43 +151,136 @@ int main(int argc, char **argv)
 
   // Allocate dA and dB, then copy the arrays A and B to the GPU
   // Allouer dA et dB, puis copier les tableaux A et B vers le GPU
-  // TODO / A FAIRE ...
+  cudaError_t cuStat;
+  cuStat = cudaMalloc((void **) &dA, N * N * sizeof(float));
+  if (cuStat != cudaSuccess) {
+    printf("L'allocation a échoué avec le message d'erreur %s\n", cudaGetErrorString(cuStat));
+    exit(1);
+  }
+  cuStat = cudaMalloc((void **) &dB, N * N * sizeof(float));
+  if (cuStat != cudaSuccess) {
+    printf("L'allocation a échoué avec le message d'erreur %s\n", cudaGetErrorString(cuStat));
+    exit(1);
+  }
+  cuStat = cudaMalloc((void **) &dC, N * N * sizeof(float));
+  if (cuStat != cudaSuccess) {
+    printf("L'allocation a échoué avec le message d'erreur %s\n", cudaGetErrorString(cuStat));
+    exit(1);
+  }
+
+  cuStat = cudaMemcpy(dA, A, N * N * sizeof(float), cudaMemcpyHostToDevice);
+  if (cuStat != cudaSuccess) {
+    printf("Le transfert a échoué avec le message d'erreur %s\n", cudaGetErrorString(cuStat));
+    exit(1);
+  }
+  cuStat = cudaMemcpy(dB, B, N * N * sizeof(float), cudaMemcpyHostToDevice);
+  if (cuStat != cudaSuccess) {
+    printf("Le transfert a échoué avec le message d'erreur %s\n", cudaGetErrorString(cuStat));
+    exit(1);
+  }
 
 
   // Call each GPU kernel appropriately to multiply matrices A and B
   // Measure and print the execution time and performance (GFlops/s) of each kernel, without counting the data transfer time
   // Appeler chaque kernel GPU de maniere appropriee pour multiplier les matrices A et B
   // Mesurer et afficher le temps d'execution et la performance (en GFlops/s) de chaque kernel, sans compter le temps de transfert.
-  // TODO / A FAIRE ...
+  auto start = std::chrono::high_resolution_clock::now();
   {
     dim3 dimGrid;
     dim3 dimBlock;
-    // multiplyMatrixGPUByBlocks<<<..., ...>>>(N);
+
+    dimBlock.x = 1;
+    dimBlock.y = 1;
+    dimBlock.z = 1;
+
+    dimGrid.x = N;
+    dimGrid.y = N;
+    dimGrid.z = 1;
+
+    // multiplyMatrixGPUByBlocks<<<dimGrid, dimBlock>>>(dA, dB, dC, N);
   }
   {
     dim3 dimGrid;
     dim3 dimBlock;
-    // multiplyMatrixGPUByBlocksThreads1D<<<..., ...>>>(N);
+
+    dimBlock.x = 64;
+    dimBlock.y = 1;
+    dimBlock.z = 1;
+
+    dimGrid.x = N / dimBlock.x;
+    dimGrid.y = N;
+    dimGrid.z = 1;
+
+    // multiplyMatrixGPUByBlocksThreads1D<<<dimGrid, dimBlock>>>(dA, dB, dC, N);
   }
   { 
     dim3 dimGrid;
     dim3 dimBlock;
-    // multiplyMatrixGPUByBlocksThreads1DNonMultiple<<<..., ...>>>(N);
+
+    dimBlock.x = 63;
+    dimBlock.y = 1;
+    dimBlock.z = 1;
+
+    dimGrid.x = (N-1)/dimBlock.x + 1;
+    dimGrid.y = N;
+    dimGrid.z = 1;
+
+    // multiplyMatrixGPUByBlocksThreads1DNonMultiple<<<dimGrid, dimBlock>>>(dA, dB, dC, N);
   }
   {
     dim3 dimGrid;
     dim3 dimBlock;
-    // multiplyMatrixGPUByBlocksThreads2D<<<..., ...>>>(N);
+
+    dimBlock.x = 64;
+    dimBlock.y = 16;
+    dimBlock.z = 1;
+
+    dimGrid.x = N / dimBlock.x;
+    dimGrid.y = N / dimBlock.y;
+    dimGrid.z = 1;
+
+    // multiplyMatrixGPUByBlocksThreads2D<<<dimGrid, dimBlock>>>(dA, dB, dC, N);
   }
   {
     dim3 dimGrid;
     dim3 dimBlock;
-    // multiplyMatrixGPUByBlocksThreads2DNonMultiple<<<..., ...>>>(N);
+
+    dimBlock.x = 27;
+    dimBlock.y = 17;
+    dimBlock.z = 1;
+
+    dimGrid.x = (N-1)/dimBlock.x + 1;
+    dimGrid.y = (N-1)/dimBlock.y + 1;
+    dimGrid.z = 1;
+
+    multiplyMatrixGPUByBlocksThreads2DNonMultiple<<<dimGrid, dimBlock>>>(dA, dB, dC, N);
   }
+
 
   // Copy the array dC back to the CPU
   // Recopier le tableau dC vers le CPU
-  // TODO / A FAIRE ...
+  cuStat = cudaMemcpy(C, dC, N * N * sizeof(float), cudaMemcpyDeviceToHost);
+  if (cuStat != cudaSuccess) {
+    printf("Le transfert de C a échoué avec le message d'erreur %s\n", cudaGetErrorString(cuStat));
+    exit(1);
+  }
+
+  auto stop = std::chrono::high_resolution_clock::now();
+
+  // Estimation du temps de transfert retour pour déduction
+  auto tr_start = std::chrono::high_resolution_clock::now();
+  cuStat = cudaMemcpy(C, dC, N * N * sizeof(float), cudaMemcpyDeviceToHost);
+  if (cuStat != cudaSuccess) {
+    printf("Le transfert de C a échoué avec le message d'erreur %s\n", cudaGetErrorString(cuStat));
+    exit(1);
+  }
+  auto tr_stop = std::chrono::high_resolution_clock::now();
+
+  auto duration = std::chrono::duration<double>((stop - start) - (tr_stop - tr_start)).count();
+  std::cout << "Le temps d'exécution du kernel est de " << duration << " secondes\n";
+
+  double gflops_s = (2.0*pow(N,3))/duration*1E-9;
+  std::cout << "Cela correspond à " << gflops_s << "GFlops/s" << std::endl;
 
   // Verify the results
   // Verifier les resultats
@@ -198,7 +293,18 @@ int main(int argc, char **argv)
 
   // Deallocate dA, dB, dC
   // Desallouer dA, dB, dC
-  // TODO / A FAIRE ...
+  cuStat = cudaFree(dA);
+  if (cuStat != cudaSuccess) {
+    printf("La libération de la memoire a echoue avec le code d'erreur \"%s\".\n", cudaGetErrorString(cuStat));
+  }
+  cuStat = cudaFree(dB);
+  if (cuStat != cudaSuccess) {
+    printf("La libération de la memoire a echoue avec le code d'erreur \"%s\".\n", cudaGetErrorString(cuStat));
+  }
+  cuStat = cudaFree(dC);
+  if (cuStat != cudaSuccess) {
+    printf("La libération de la memoire a echoue avec le code d'erreur \"%s\".\n", cudaGetErrorString(cuStat));
+  }
 
   return 0;
 }
